e7ca8cf4b7c4c8e97f176da18e94682d453ce0c1
agarc14 <70459224+agarc14@users.noreply.github.com>
2025-03-20 15:31:21 +0000

    end of class

---
+++ tapweb/src/main/java/com/example/tapweb/HiController.java
@@ -0,0 +1,31 @@
+package com.example.tapweb;
+
+import java.awt.List;
+import java.util.ArrayList;
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HiController {
+
+    private static String sendtmplt = "R: %s";
+    private final AtomicLong counter = new AtomicLong();
+    private final List<Details> detailsList = new ArrayList<>();
+
+    @RequestMapping("/")
+    public String index(){
+        return "Hello there. I'm from Spring Boot";
+    }
+
+    @RequestMapping("/add-det")
+    @ResponseBody
+    public Details addDetails(@RequestParam(name="name", required=false, defaultValue="Unknown") String name) {
+        Details detail = new Details(counter.incrementAndGet(), name);
+        detailsList.add(detail);
+        return detail;
+    }
+}
